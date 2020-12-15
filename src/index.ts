9d8dc487eff1e75730ef6612a3228ed5457fe2f9
WISUNGWON <tkrhkaos12@naver.com>
2020-12-15 12:10:01 +0900

    using object in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-const sayHi = (name:string, age:number, gender:string):string => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+interface Human {
+    name:string,
+    age:number,
+    gender:string
+};
+
+const person = {
+    name:"sungwon",
+    gender: "male",
+    age:22
+};
+
+const sayHi = (person:Human):string => {
+    console.log(`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`);
     return "heelo";
 };
 
-console.log(sayHi("sungwon",444, "sngwon"));
+console.log(sayHi(person));
 
 export {};
